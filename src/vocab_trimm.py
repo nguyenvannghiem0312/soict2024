d514fdf22a91be0b7842270f8f9596af531af68b
nghiemnv <nguyenvannghiem0312@gmail.com>
2024-11-22 14:31:50 +0000

    chore: trimm vocab config

--- src/vocab_trimm.py
+++ src/vocab_trimm.py
@@ -1,33 +1,38 @@
 import vocabtrimmer
+from utils.preprocess import TextPreprocessor
+from utils.io import read_json_or_dataset, save_to_json
+from utils.dataset import search_by_id
+from tqdm import tqdm
+from bm25s.hf import BM25HF
+import json
+import argparse
 
-trimmer = vocabtrimmer.VocabTrimmer("jinaai/jina-reranker-v2-base-multilingual", double_embedding=False)
-trimmer.trim_vocab(
-        path_to_save="jina-reranker-v2-base-multilingual",
-        language="vi",
-        dataset='Turbo-AI/data-corpus',
-        dataset_column='text',
-        dataset_split='train',
-        target_vocab_size=None,
-        min_frequency=2,
-        chunk=500,
-        cache_file_vocab=None,
-        cache_file_frequency=None,
-        overwrite=False
+def trim_vocab_model(config):
+    trimmer = vocabtrimmer.VocabTrimmer(config["model"], double_embedding=False)
+    trimmer.trim_vocab(
+            path_to_save=config["path_to_save"],
+            language="vi",
+            dataset=config["dataset"],
+            dataset_column=config["dataset_column"],
+            dataset_split=config["dataset_split"],
+            target_vocab_size=None,
+            min_frequency=2,
+            chunk=500,
+            cache_file_vocab=None,
+            cache_file_frequency=None,
+            overwrite=False
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run BM25 pipeline with configuration.")
+    parser.add_argument(
+        "--config_path", 
+        type=str, 
+        default="src/configs/trimm_vocab_config.json", 
+        help="Path to the configuration JSON file."
     )
-
-from transformers import AutoModel, AutoTokenizer
-import torch
-
-# Load the model and tokenizer
-model = AutoModel.from_pretrained('jina-reranker-v2-base-multilingual')
-tokenizer = AutoTokenizer.from_pretrained('jina-reranker-v2-base-multilingual')
-
-# Convert the model to bf16
-model = model.to(torch.bfloat16)
-
-# Define your model repository name (it should be unique)
-repo_name = "Turbo-AI/jina-reranker-v2-base-multilingual__trim_vocab"
-
-# Push the model and tokenizer to the Hugging Face Hub
-model.push_to_hub(repo_name)
-tokenizer.push_to_hub(repo_name)
+    
+    args = parser.parse_args()
+    config = read_json_or_dataset(args.config_path)
+    trim_vocab_model(config)
